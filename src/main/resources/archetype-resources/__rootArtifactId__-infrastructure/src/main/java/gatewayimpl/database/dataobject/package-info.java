eed9724d9743590e80e6b0e2925cc775a7e15936
Vincent Vic <Vic.Coding@gmail.com>
2022-09-25 15:11:00 +0800

    init

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-infrastructure/src/main/java/gatewayimpl/database/dataobject/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <h1>基础设施 - 数据库DO对象</h1>
+ *
+ * @version 1.0
+ * @author Vincent Vic
+ */
+package $package.gatewayimpl.database.dataobject;
---
+++ src/main/resources/archetype-resources/__rootArtifactId__-infrastructure/src/main/java/gatewayimpl/database/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <h1>基础设施 - 数据库</h1>
+ *
+ * @version 1.0
+ * @author Vincent Vic
+ */
+package $package.gatewayimpl.database;
